5680b46b4fe4a0eab493b8efd85e4443b7ced4c8
MjrKong42 <mjr.kong42@gmail.com>
2021-02-23 11:17:54 -0600

    JavaScript Variables Exercise w/ Bonus

---
+++ variablesExercise.js
@@ -0,0 +1,62 @@
+// ONE
+
+// Variable that will not changes uses CONST
+
+// TWO
+
+let num=20;
+console.log(num);
+
+// THREE
+
+num=15;
+console.log(num);
+
+// FOUR
+
+num=num+7;
+console.log(num);
+
+num=15;
+num+=7;
+console.log(num);
+
+// FIVE
+
+num--;
+console.log(num);
+
+// SIX
+
+num="This is NOT a Number";
+console.log(num);
+
+// SEVEN
+
+let blank;
+
+// EIGHT
+
+console.log(blank);
+
+// NINE
+
+blank=null;
+console.log(blank);
+
+// BONUS ONE
+
+let quarter=0.25, dime=0.10, nickel=0.05, penny=0.01;
+
+// BONUS TWO
+
+console.log(quarter,dime,nickel,penny);
+
+// BONUS THREE
+
+let letters="qwertyuiop"
+console.log(letters);
+
+// BONUS FOUR
+
+console.log(letters.charAt(letters.length-1));
